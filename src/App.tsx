7e7923275a6dc1b4a9096b3e0ed4d9c284bd9605
Higor Lopes <higorlopes@42codelab.com>
2022-06-26 09:52:16 -0300

    make it better

--- src/App.tsx
+++ src/App.tsx
@@ -10,36 +10,96 @@ import { BoardScketcher } from "./Drawings/BoardSketcher";
 import { PlayerSketcher } from "./Drawings/PlayerSketcher";
 import { PlayerStatus } from "./Domain/Players/Enums/PlayerStatus";
 import useWebSocket from "react-use-websocket";
-import { create } from "domain";
-
+import { io } from "socket.io-client";
 var board!: Board;
 var players!: Player[];
 var player!: Player;
 
 function App() {
 	const ref = useRef<any>();
+	const videoRef = useRef<any>();
+	const videoRef2 = useRef<any>();
+	const canvasRef = useRef<any>();
 
 	var boardScketcher = new BoardScketcher();
 	var playerSketcher = new PlayerSketcher();
 
-	const ws = useWebSocket("ws://localhost:8998", {
-		onOpen: (c) => {
-			console.log(`Connected to App WS`);
-		},
-		onMessage: (m) => {
-			board = Object.assign(Board.empty(), JSON.parse(m.data).board);
-			players = Object.assign([], JSON.parse(m.data).players);
+	var videoSocket = io("ws://pacweserver.herokuapp.com");
+	// var videoSocket = io("ws://localhost:3000");
+	videoSocket.connect();
 
-			if (!player)
-				player = Object.assign(Player.empty(), JSON.parse(m.data).self);
-		},
-		queryParams: { token: "123456" },
-		onError: (event) => {
-			console.error(event);
-		},
-		shouldReconnect: (closeEvent) => true,
-		reconnectInterval: 3000,
+	videoSocket.on("connect", () => {
+		console.log("Connected");
 	});
+
+	videoSocket.on("video", (d) => {
+		videoRef2.current.src = d.video;
+	});
+
+	function Draw(video: any, context: any) {
+		context?.drawImage(video, 0, 0, context.width, context.height);
+		videoSocket.emit("video", canvasRef.current.toDataURL("image/webp"));
+	}
+
+	function loadCamera(stream: any) {
+		try {
+			videoRef.current.srcObject = stream;
+		} catch (error) {
+			videoRef.current.src = URL.createObjectURL(stream);
+		}
+	}
+
+	useEffect(() => {
+		navigator.mediaDevices.getUserMedia =
+			navigator.mediaDevices.getUserMedia ||
+			(navigator.mediaDevices as any)?.webkitGetUserMedia ||
+			(navigator.mediaDevices as any)?.mozGetUserMedia ||
+			(navigator.mediaDevices as any)?.msgGetUserMedia;
+
+		if (navigator.mediaDevices.getUserMedia) {
+			navigator.mediaDevices
+				.getUserMedia({
+					video: true,
+					audio: false,
+				})
+				.then((stream) => {
+					loadCamera(stream);
+				});
+		}
+
+		canvasRef.current.width = 200;
+		canvasRef.current.height = 200;
+		const video = videoRef.current;
+		const context = canvasRef.current.getContext("2d");
+
+		context.width = 200;
+		context.height = 200;
+
+		setInterval(function () {
+			Draw(video, context);
+		}, 100);
+	}, []);
+
+	// videoSocket
+
+	// const ws = useWebSocket("", {
+	// 	onOpen: (c) => {
+	// 		console.log(`Connected to App WS`);
+	// 	},
+	// 	onMessage: (m) => {
+	// 		board = Object.assign(Board.empty(), JSON.parse(m.data).board);
+	// 		players = Object.assign([], JSON.parse(m.data).players);
+
+	// 		if (!player)
+	// 			player = Object.assign(Player.empty(), JSON.parse(m.data).self);
+	// 	},
+	// 	queryParams: { token: "123456" },
+	// 	onError: (event) => {
+	// 		console.error(event);
+	// 	},
+	// 	shouldReconnect: (closeEvent) => true,
+	// 	reconnectInterval: 3000,
+	// });
 
 	const Sketch = (p: p5) => {
 		p.preload = () => {};
@@ -57,13 +117,13 @@ function App() {
 			playerSketcher.draw(p, players);
 
 			p.keyPressed = (e: any) => {
-				ws.sendMessage(
-					JSON.stringify({
-						self: player,
-						type: "MOVE",
-						key: e.code,
-					})
-				);
+				// ws.sendMessage(
+				// 	JSON.stringify({
+				// 		self: player,
+				// 		type: "MOVE",
+				// 		key: e.code,
+				// 	})
+				// );
 			};
 		};
 	};
@@ -74,8 +134,13 @@ function App() {
 
 	return (
 		<div style={{ display: "flex" }}>
+			<video ref={videoRef} autoPlay muted height={200} width={200} />
+			<div>DEVESAO</div>
+			<img ref={videoRef2} height={200} width={200} />
+			<canvas ref={canvasRef} style={{ display: "none" }} id="preview" />
+
 			<button onClick={create}>Connect</button>
-			<div ref={ref} />
+			{/* <div ref={ref} /> */}
 		</div>
 	);
 }
