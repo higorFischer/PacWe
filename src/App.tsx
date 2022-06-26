33bcd3e9eb8b5eab76d031c4434bba8c0f73d418
Higor Lopes <higorlopes@42codelab.com>
2022-06-26 12:48:52 -0300

    published

--- src/App.tsx
+++ src/App.tsx
@@ -2,26 +2,22 @@ import { useEffect, useRef, useState } from "react";
 import p5 from "p5";
 import { Board } from "./Domain/Boards/Entities/Board";
 import { Player } from "./Domain/Players/Entities/Player";
-import { PlayerMover } from "./Domain/Players/Services/PlayerMover";
-import { BoardBuilder } from "./Domain/Boards/Services/BoardBuilder";
-import { BoardMovementVerifier } from "./Domain/Boards/Services/BoardMovementVerifier";
-import { board1 } from "./Boards/stringBoards";
 import { BoardScketcher } from "./Drawings/BoardSketcher";
 import { PlayerSketcher } from "./Drawings/PlayerSketcher";
-import { PlayerStatus } from "./Domain/Players/Enums/PlayerStatus";
-import useWebSocket from "react-use-websocket";
-import { io, Socket } from "socket.io-client";
-import { ImageSquare } from "./Components/ImageSquare";
-import { connect } from "http2";
+import { io } from "socket.io-client";
+
 var board!: Board;
 var players!: Player[];
 var player!: Player;
 
 function App() {
 	const [videoSocket, useVideoSocket] = useState(
-		io("wss://pacweserver.herokuapp.com")
-		// io("ws://localhost:3001")
+		// io("wss://pacweserver.herokuapp.com")
+		io("ws://localhost:3001")
+		// io("ws://localhost:8000")
 	);
+
+	const ws = new WebSocket("wss://localhost:8000");
 
 	const ref = useRef<any>();
 	const videoRef = useRef<any>();
@@ -46,16 +42,16 @@ function App() {
 		if (!player) player = Object.assign(Player.empty(), obj.self);
 	});
 
-	// videoSocket.on("gameinit", (m) => {
-	// 	const obj = JSON.parse(m);
+	videoSocket.on("gameinit", (m) => {
+		const obj = JSON.parse(m);
 
-	// 	board = Object.assign(Board.empty(), obj.board);
-	// 	players = Object.assign([], obj.players);
+		board = Object.assign(Board.empty(), obj.board);
+		players = Object.assign([], obj.players);
 
-	// 	if (!player) player = Object.assign(Player.empty(), obj.self);
+		if (!player) player = Object.assign(Player.empty(), obj.self);
 
-	// 	new p5(Sketch, ref.current);
-	// });
+		new p5(Sketch, ref.current);
+	});
 
 	videoSocket.on("video", (d: any) => {
 		setVideos((videos: any) => {
@@ -64,25 +60,6 @@ function App() {
 			return v;
 		});
 	});
-
-	// const ws = useWebSocket("", {
-	// 	onOpen: (c) => {
-	// 		console.log(`Connected to App WS`);
-	// 	},
-	// 	onMessage: (m) => {
-	// 		board = Object.assign(Board.empty(), JSON.parse(m.data).board);
-	// 		players = Object.assign([], JSON.parse(m.data).players);
-
-	// 		if (!player)
-	// 			player = Object.assign(Player.empty(), JSON.parse(m.data).self);
-	// 	},
-	// 	queryParams: { token: "123456" },
-	// 	onError: (event) => {
-	// 		console.error(event);
-	// 	},
-	// 	shouldReconnect: (closeEvent) => true,
-	// 	reconnectInterval: 3000,
-	// });
 
 	function Draw(video: any, context: any) {
 		context?.drawImage(video, 0, 0, context.width, context.height);
@@ -137,7 +114,7 @@ function App() {
 
 		p.draw = () => {
 			boardScketcher.draw(p, board);
-			playerSketcher.draw(p, players);
+			playerSketcher.draw(p, players, videos[0]);
 
 			p.keyPressed = (e: any) => {
 				videoSocket.emit(
@@ -156,6 +133,10 @@ function App() {
 		videoSocket.emit("game");
 	};
 
+	const reset = () => {
+		videoSocket.emit("reset");
+	};
+
 	return (
 		<div style={{ display: "flex" }}>
 			{/* <video ref={videoRef} autoPlay muted height={200} width={200} />
@@ -166,9 +147,11 @@ function App() {
 						<ImageSquare key={videoKey} data={videos[videoKey]} />
 					);
 				})}
-			<canvas ref={canvasRef} style={{ display: "none" }} id="preview" /> */}
+			*/}
+			<canvas ref={canvasRef} style={{ display: "none" }} id="preview" />
 
 			<button onClick={create}>Connect</button>
+			<button onClick={reset}>Reset</button>
 			<div ref={ref} />
 		</div>
 	);
